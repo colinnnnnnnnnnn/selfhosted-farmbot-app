a2b9d6914cab876147760dbf5e8c1b6cb4a0b282
Slice277353 <ilia.covali@gmail.com>
2025-11-28 10:26:06 +0200

    made an auditlog model

--- api/models.py
+++ api/models.py
@@ -27,14 +27,13 @@ class Sequence(models.Model):
     def __str__(self):
         return self.name
 
-class Step(models.Model):
-    sequence = models.ForeignKey(Sequence, related_name='steps', on_delete=models.CASCADE)
-    order = models.PositiveIntegerField()
-    command = models.CharField(max_length=100)
-    parameters = models.JSONField(default=dict)
 
-    class Meta:
-        ordering = ['order']
+class AuditLog(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    action = models.CharField(max_length=100)
+    object_id = models.CharField(max_length=100, blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f'{self.sequence.name} - Step {self.order} - {self.command}'
+        return f"{self.timestamp} - {self.user} - {self.action}"
