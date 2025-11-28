5dea29f2241d435cb66707e2452714a2d4fb2591
Slice277353 <ilia.covali@gmail.com>
2025-11-28 11:44:27 +0200

    added url for notification preference

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,8 @@ from .views import (
     lua_script_view, get_position_view, send_message_view, take_photo_view,
     register_view, login_view, logout_view, me_view, water_plant_view,
     mount_tool_view, dismount_tool_view, dispense_view, clear_photos_view,
-    seed_injector_view, rotary_tool_view, soil_sensor_view, weeder_view
+    seed_injector_view, rotary_tool_view, soil_sensor_view, weeder_view,
+    notification_preference_view
 )
 
 router = DefaultRouter()
@@ -48,4 +49,5 @@ urlpatterns = [
     path('rotary-tool/', rotary_tool_view, name='rotary-tool'),
     path('soil-sensor/', soil_sensor_view, name='soil-sensor'),
     path('weeder/', weeder_view, name='weeder'),
+    path('notification-preferences/', notification_preference_view, name='notification-preferences'),
 ]
